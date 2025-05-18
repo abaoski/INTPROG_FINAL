f491f13db9b962ef91c377316be49cb5790e3f17
shekainah-g <shekainahgaceta029@gmail.com>
2025-05-19 00:13:06 +0800

    backend

---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+
+export const environment = {
+  production: false,
+  apiUrl: "https://intprog-final-hak0.onrender.com  ",
+  useFakeBackend: false  // Use real backend in development
+}; 
